7dba42c5a8cc6e19f87c6323ea3e5cdb11fd5e1f
Luis M. B. Varona <lbvarona@mta.ca>
2026-01-15 23:06:41 -0400

    Combine tax and exp initial plot scripts into one

--- sandbox/1_initial_plots/initial_plots.py
+++ sandbox/1_initial_plots/initial_plots.py
@@ -16,7 +16,6 @@ import seaborn as sns
 # %%
 WD = Path(__file__).parent
 SRC = WD.parent.parent / "data" / "data_final" / "data_master.xlsx"
-TEX_DIR = WD / "tex"
 PLOTS_DIR = WD / "plots"
 DIVIDE_YEAR = 2012
 
@@ -26,12 +25,95 @@ def main() -> None:
     df = pl.read_excel(SRC).select(
         [
             "Year",
+            "PolExpCapita",
             "AvgTaxRate",
             "Provider_PPSA",
             "LatestCensusPop",
         ]
     )
 
+    PLOTS_DIR.mkdir(parents=True, exist_ok=True)
+
+    plot_polexpcapita(df)
+    plot_avgtaxrate(df)
+
+
+# %%
+def plot_polexpcapita(df: pl.DataFrame) -> None:
+    df_weighted = (
+        df.group_by("Year")
+        .agg(
+            (
+                (pl.col("PolExpCapita") * pl.col("LatestCensusPop"))
+                .filter(pl.col("Provider_PPSA"))
+                .sum()
+                / pl.col("LatestCensusPop").filter(pl.col("Provider_PPSA")).sum()
+            ).alias("PolExpCapita_PPSA"),
+            (
+                (pl.col("PolExpCapita") * pl.col("LatestCensusPop"))
+                .filter(~pl.col("Provider_PPSA"))
+                .sum()
+                / pl.col("LatestCensusPop").filter(~pl.col("Provider_PPSA")).sum()
+            ).alias("PolExpCapita_nonPPSA"),
+        )
+        .sort("Year")
+        .with_columns(
+            (
+                (pl.col("PolExpCapita_PPSA") - pl.col("PolExpCapita_PPSA").shift(1))
+                / pl.col("PolExpCapita_PPSA").shift(1)
+                * 100
+            ).alias("PolExpCapita_PPSA_yoy"),
+            (
+                (
+                    pl.col("PolExpCapita_nonPPSA")
+                    - pl.col("PolExpCapita_nonPPSA").shift(1)
+                )
+                / pl.col("PolExpCapita_nonPPSA").shift(1)
+                * 100
+            ).alias("PolExpCapita_nonPPSA_yoy"),
+            (pl.col("PolExpCapita_nonPPSA") - pl.col("PolExpCapita_PPSA")).alias(
+                "PolExpCapita_diff"
+            ),
+        )
+    )
+
+    years_w = get_vals(df_weighted, "Year").astype(int)
+
+    plt.figure()
+    sns.lineplot(x=years_w, y=get_vals(df_weighted, "PolExpCapita_PPSA"), label="PPSA")
+    sns.lineplot(
+        x=years_w, y=get_vals(df_weighted, "PolExpCapita_nonPPSA"), label="non-PPSA"
+    )
+    plt.title("PolExpCapita")
+    add_divider(plt.gca())
+    plt.xticks(years_w, rotation=90)
+    plt.savefig(PLOTS_DIR / "pec_weighted.png")
+    plt.close()
+
+    plt.figure()
+    sns.lineplot(
+        x=years_w, y=get_vals(df_weighted, "PolExpCapita_PPSA_yoy"), label="PPSA"
+    )
+    sns.lineplot(
+        x=years_w, y=get_vals(df_weighted, "PolExpCapita_nonPPSA_yoy"), label="non-PPSA"
+    )
+    plt.title("YoY PolExpCapita (%)")
+    add_divider(plt.gca())
+    plt.xticks(years_w, rotation=90)
+    plt.savefig(PLOTS_DIR / "pec_weighted_yoy.png")
+    plt.close()
+
+    plt.figure()
+    sns.lineplot(x=years_w, y=get_vals(df_weighted, "PolExpCapita_diff"))
+    plt.title("non-PPSA - PPSA Difference")
+    add_divider(plt.gca())
+    plt.xticks(years_w, rotation=90)
+    plt.savefig(PLOTS_DIR / "pec_weighted_diff.png")
+    plt.close()
+
+
+# %%
+def plot_avgtaxrate(df: pl.DataFrame) -> None:
     df_unweighted = (
         df.drop("LatestCensusPop")
         .group_by("Year")
@@ -99,8 +181,6 @@ def main() -> None:
 
     years_unw = get_vals(df_unweighted, "Year").astype(int)
     years_w = get_vals(df_weighted, "Year").astype(int)
-
-    PLOTS_DIR.mkdir(parents=True, exist_ok=True)
 
     plt.figure()
     sns.lineplot(
